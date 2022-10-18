528d9041ab16e11a27638a33ba1897098609c073
KathleenJung <ovo0325@outlook.kr>
2022-10-18 21:22:46 +0900

    create app bar

---
+++ pages/Appbar.tsx
@@ -0,0 +1,37 @@
+import { NextPage } from "next";
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const SelectedPage: NextPage = () => {
+    return (
+      <>
+        <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={3}>
+          <Item><Button color="inherit"><ArrowBackIcon></ArrowBackIcon>BACK TO MENU</Button></Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid xs={3}>
+          <Item><Button color="inherit"><ArrowBackIcon></ArrowBackIcon>ORDER STATUS</Button></Item>
+        </Grid>
+      </Grid>
+    </Box>
+      </>
+      );
+}
+
+export default SelectedPage;
---
+++ pages/selectMenu.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import type { NextPage } from 'next';
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import Appbar from "./Appbar";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+  
+const SelectMenu: NextPage = () => {
+  return (
+      <>
+          <Appbar></Appbar>
+        <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={3}>
+          <Item><Button color="inherit"><ArrowBackIcon></ArrowBackIcon>BACK TO MENU</Button></Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid xs={3}>
+          <Item><Button color="inherit"><ArrowBackIcon></ArrowBackIcon>ORDER STATUS</Button></Item>
+        </Grid>
+      </Grid>
+    </Box>
+      </>
+  );
+};
+
+export default SelectMenu;
