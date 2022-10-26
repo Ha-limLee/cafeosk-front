99f6b4777ea650e644095a23944430a91b6f5ddb
KathleenJung <ovo0325@outlook.kr>
2022-10-27 01:15:30 +0900

    Order Status UI 수정

--- src/components/order-drawer.tsx
+++ src/components/order-drawer.tsx
@@ -1,17 +1,46 @@
 import * as React from "react";
 import type { NextPage } from "next";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import RoomServiceIcon from "@mui/icons-material/RoomService";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineDot from "@mui/lab/TimelineDot";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 type Anchor = "right";
+
+const steps = [
+  {
+    label: "Ordered",
+    description: `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`,
+  },
+  {
+    label: "Preparing",
+    description: "An ad group contains one or more ads which target a shared set of keywords.",
+  },
+  {
+    label: "Finishing",
+    description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+  },
+  {
+    label: "Served",
+    description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+  },
+];
 
 const drawer: NextPage = () => {
   const [state, setState] = React.useState({
@@ -34,55 +63,98 @@ const drawer: NextPage = () => {
       setState({ ...state, [anchor]: open });
     };
 
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   const list = (anchor: Anchor) => (
     <Box
-      sx={{ width: "auto" }}
+      sx={{ maxWidth: 400, mx: 3 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <h1>Order Status</h1>
-      <Timeline
-        sx={{
-          [`& .${timelineItemClasses.root}:before`]: {
-            flex: 0,
-            padding: 0,
-          },
-        }}
-      >
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Ordered</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Preparing</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Finishing</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>Served</TimelineContent>
-        </TimelineItem>
-      </Timeline>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel
+              optional={index === 2 ? <Typography variant="caption">Last step</Typography> : null}
+            >
+              {step.label}
+            </StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                    {index === steps.length - 1 ? "Finish" : "Continue"}
+                  </Button>
+                  <Button disabled={index === 0} onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                    Back
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+
+      <Grid container spacing={2} columns={16} sx={{mt:5}}>
+        <Grid item xs={8}>
+<Typography>Subtotal</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography>$456.90</Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} columns={16}>
+        <Grid item xs={8}>
+          <Typography>Discount -10%</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography>$45.69</Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} columns={16}>
+        <Grid item xs={8}>
+<h2>Total</h2>
+        </Grid>
+        <Grid item xs={8}>
+          <h2>$411.21</h2>
+        </Grid>
+      </Grid>
     </Box>
   );
 
   return (
-    <div>
+    <>
       {(["right"] as const).map((anchor) => (
         <React.Fragment key={"anchor"}>
           <Button color="inherit" onClick={toggleDrawer(anchor, true)}>
@@ -93,7 +165,7 @@ const drawer: NextPage = () => {
           </Drawer>
         </React.Fragment>
       ))}
-    </div>
+    </>
   );
 };
 
